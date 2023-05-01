6ab9609cad0cdbbe71ff6cbbb7728362bfa3742d
AhrensOG <ahrensgabriel09@gmail.com>
2023-05-01 18:47:22 -0300

    Fix bugs for deploy

--- api/index.js
+++ api/index.js
@@ -19,10 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const port = process.env.PORT || '8080'
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen( port, () => {
+    console.log('%s listening at ', port); // eslint-disable-line no-console
   });
 });
